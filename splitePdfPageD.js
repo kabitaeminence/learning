0bc9b5ab2e8e2bd42d10b25a59c33226e7375030
kabita <98816015+kabitaeminence@users.noreply.github.com>
2022-05-11 16:19:47 +0530

    Add files via upload

---
+++ splitePdfPageD.js
@@ -0,0 +1,25 @@
+const { fileURLToPath } = require("url");
+
+async function splitPdf(url) {
+  var convertapi = require("convertapi")("HdyHQpcMuL44xmgr");
+  const splitdata = await convertapi.convert(
+    "split",
+    {
+      File: url,
+    },
+    "pdf"
+  );
+
+  const data = await splitdata.response.Files;
+  const finalArr = [];
+  const arrOfPages = data.map((val, i) => {
+    const data = { Url: val.Url, pageNo: i + 1 };
+    finalArr.push(data);
+  });
+  console.log(finalArr);
+}
+// console.log(
+splitPdf(
+  "https://stagingdbpscasting.fra1.cdn.digitaloceanspaces.com/3d3ddbd-158c-d08-1f-772d2571052.pdf"
+);
+// );
